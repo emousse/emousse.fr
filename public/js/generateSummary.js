c4b6eb3af49883e7e365611335cb661327a5694c
Etienne Moussé <etiennemousse@MacBook-Pro-de-Etienne.local>
2019-02-18 17:40:23 +0100

    adding dark theme

--- public/js/generateSummary.js
+++ public/js/generateSummary.js
@@ -1,17 +1,27 @@
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    document.getElementById("myBar").style.width = scrolled + "%";
+}
 $(function () {
     $arrayH1 = $('.card-body h1');
-    $summary = $('<div id="list-exemple" class="list-group summary-sticky"></div>')
+    $summary = $('<ul id="list-exemple" class="nav justify-content-center"></ul>')
     
     $arrayH1.each(function (index, element) {
         console.log(index, element);
-        $newA = $('<a class="list-group-item list-group-item-action"></a>')
+        $newLi = $('<li class="nav-item"></li>')
+        $newA = $('<a class="nav-link"></a>')
         var idAttr = '#' + $(element).attr('id');
         var idText = $(element).text();
         $newA.attr('href', idAttr);
         $newA.text(idText);
-        $summary.append($newA);
+        $newLi.append($newA)
+        $summary.append($newLi);
     });
     if($summary){
-        $('.summary').append($summary);
+        $('.fixed-bottom').prepend($summary);
     };
 });
